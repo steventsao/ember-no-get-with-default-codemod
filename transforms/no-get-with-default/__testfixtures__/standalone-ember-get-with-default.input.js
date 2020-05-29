9cfac896154579f8ae50c10eb3eff365096d2d2d
Steven Tsao <steventsao713@gmail.com>
2020-05-29 01:20:53 -0700

    Add interpolated paths in tests

--- transforms/no-get-with-default/__testfixtures__/standalone-ember-get-with-default.input.js
+++ transforms/no-get-with-default/__testfixtures__/standalone-ember-get-with-default.input.js
@@ -1,2 +1,3 @@
 const test = getWithDefault(this, "key", "default");
 const test2 = getWithDefault(this, 'key', 'default').includes('blah');
+const test3 = getWithDefault(obj, `foo.${key}`, []);
--- transforms/no-get-with-default/__testfixtures__/standalone-ember-get-with-default.output.js
+++ transforms/no-get-with-default/__testfixtures__/standalone-ember-get-with-default.output.js
@@ -1,2 +1,3 @@
 const test = get(this, "key") !== undefined ? get(this, "key") : "default";
 const test2 = (get(this, 'key') !== undefined ? get(this, 'key') : 'default').includes('blah');
+const test3 = get(obj, `foo.${key}`) !== undefined ? get(obj, `foo.${key}`) : [];
